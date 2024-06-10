afc111f183607ae9a4db74887b75416e4bb04855
Kévin <kevin.bejot@gmail.com>
2024-06-10 16:49:59 +0200

    cleaning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from "./article/article.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-root',
@@ -9,7 +10,8 @@ import { ArticleComponent } from "./article/article.component";
     styleUrl: './app.component.css',
     imports: [
       RouterOutlet,
-      ArticleComponent
+      ArticleComponent,
+      CommonModule,
       ]
 })
 export class AppComponent {
@@ -22,5 +24,33 @@ export class AppComponent {
   onAffiche(arg:string) {
     return this.message = "Merci d'avoir voté pour l'article : " + arg;
   }
+
+  list = [
+  {
+    titleArticle: "Vélo", 
+    prixArticle:230,
+    available:false
+  },
+  {
+    titleArticle: "Voiture", 
+    prixArticle:50,
+    available:true
+  },
+  {
+    titleArticle: "Bus", 
+    prixArticle:1000,
+    available:false
+  },
+  {
+    titleArticle: "Trotinette", 
+    prixArticle:75,
+    available:true
+  },
+  {
+    titleArticle: "Skateboard", 
+    prixArticle:10,
+    available:true
+  },
+]
 
 }
